4b7bc0cee19adc8453acdc5a334bde5c812e6b31
AndrewFyy <Yue930315@gmail.com>
2020-10-27 23:56:38 +0800

    Create SingleDimensionArrayMain.java

---
+++ src/Array/SingleDimensionArrayMain.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class SingleDimensionArrayMain {
+	public static void main(String args[] ){
+		 int arr[] = null;
+		 
+		 //constructor
+		 public SingleDimesionArray(int sizeOfArray)
+	}		//end of Main method
+
+}		//end of Class
